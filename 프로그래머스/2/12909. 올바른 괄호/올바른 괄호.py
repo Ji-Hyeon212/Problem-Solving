77521bb6d740073f9d7adbf053757b1e01a73dda
jihyeon <wlgus7464@gmail.com>
2025-03-29 23:49:39 +0900

    [level 2] Title: 올바른 괄호, Time: 10.38 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = True
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        if i == ')':
+            if len(stack)==0: answer=False
+            else: stack.pop()
+    if len(stack)!=0 : answer = False
+            
+    return answer
