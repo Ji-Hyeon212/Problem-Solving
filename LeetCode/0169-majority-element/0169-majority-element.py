39859b7ae5c9391f200a951ad947abf24abc2c56
Jihyeon <wlgus7464@gmail.com>
2025-05-22 16:31:16 +0900

    Time: 6 ms (72.55%), Space: 13.7 MB (31.19%) - LeetHub

---
+++ LeetCode/0169-majority-element/0169-majority-element.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        majority = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                majority = num
+                count += 1
+            else:
+                if num == majority:
+                    count+=1
+                else:
+                    count-=1
+
+        return majority
+        
