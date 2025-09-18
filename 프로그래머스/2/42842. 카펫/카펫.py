db892ea8fe61b5438850432d038e746f237d340d
Jihyeon Lee <wlgus7464@gmail.com>
2025-09-18 20:25:28 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,14 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    # 가로 또는 세로는 total의 root 값보다 크다.
+    # int(root) + 1 을 하면 최대 가로 또는 세로 크기
+    for height in range(1, int(total ** 0.5) + 1):
+        if (total%height == 0):
+            width = total // height
+            yello_width = width - 2
+            yello_height = height - 2
+            # y_w와 y_h 곱이 넓이와 같으면
+            if (yello_width * yello_height == yellow):
+                return [width, height]
+    
+    
