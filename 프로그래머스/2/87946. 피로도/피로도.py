5ae713d520a4e94ba9f286b48d556390da0c9e44
Jihyeon Lee <wlgus7464@gmail.com>
2025-10-13 12:16:29 +0900

    [level 2] Title: 피로도, Time: 51.54 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -2,21 +2,25 @@ from itertools import permutations
 
 def solution(k, dungeons):
     n = len(dungeons)
+    visited = [False] * n
+    global max_count 
     max_count = 0
-    for dungeon_seq in permutations(dungeons, n):
-        current_k = k
-        count = 0
-        for min_fatigue, cost in dungeon_seq:
-            # 현재 피로도가 최소 피로도 이상이면 탐험(피로도 차감)
-            if current_k >= min_fatigue:
-                current_k -= cost
-                count += 1
-            else:
-                break
-        #최대 던전 수 갱신
+
+    def backtrack(current_k, count):
+        global max_count
         max_count = max(max_count, count)
         
-        if max_count == n:
-            return n
+        if count == n:
+            return
+        
+        for i in range(n):
+            min_fatigue, cost = dungeons[i]
+            # 아직 방문 전이고 현재 피로도가 최소 피로도 이상인 경우 전진
+            if not visited[i] and current_k >= min_fatigue:
+                visited[i] = True
+                backtrack(current_k - cost, count + 1)
+                visited[i] = False
+        
+    backtrack(k, 0)
     
     return max_count
