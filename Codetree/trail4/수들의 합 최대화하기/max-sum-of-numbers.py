4404497eed8e6387d012e651f70f7cf92305e225
Jihyeon Lee <wlgus7464@gmail.com>
2026-09-09 23:20:13 +0900

    [Passed] 수들의 합 최대화하기 (103ms, 19MB)

---
+++ Codetree/trail4/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,24 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [False] * n
+answer = -float('inf')
+
+
+def dfs(row, total):
+    global answer
+
+    if row == n:
+        answer = max(answer, total)
+        return
+
+    for col in range(n):
+        if not visited[col]:
+            visited[col] = True
+            dfs(row + 1, total + grid[row][col])
+            visited[col] = False
+
+
+dfs(0, 0)
+
+print(answer)
